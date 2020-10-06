92844c90c009ac5ce60c99e7ae05717625ab3de0
Heinrich <hscheepers10@gmail.com>
2020-10-06 11:43:34 +0200

    Added code.
    initial code.

---
+++ Java Project/app/src/main/java/com/example/bankingapp/login.java
@@ -0,0 +1,28 @@
+package com.example.bankingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class login extends AppCompatActivity {
+
+    private TextView here;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        here = (TextView)findViewById(R.id.regHereTV);
+        here.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login.this,register.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ Java Project/app/src/main/java/com/example/bankingapp/register.java
@@ -0,0 +1,29 @@
+package com.example.bankingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class register extends AppCompatActivity {
+
+    private TextView here;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+
+        here = (TextView) findViewById(R.id.logHereTV);
+        here.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(register.this,login.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
