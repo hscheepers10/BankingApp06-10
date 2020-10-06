b1b38c670c75d302d6d867f6516a9f82e5369e9b
Heinrich <hscheepers10@gmail.com>
2020-10-06 14:57:35 +0200

    Edit
    Push before attempting changes.

--- Java Project/app/src/main/java/com/example/bankingapp/login.java
+++ Java Project/app/src/main/java/com/example/bankingapp/login.java
@@ -4,20 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class login extends AppCompatActivity {
 
     private TextView here;
     private Button logInBtn;
+    private EditText emailET,passwordET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
+        //On click listener for registration activity.
         here = (TextView)findViewById(R.id.regHereTV);
         here.setOnClickListener(new View.OnClickListener() {
 
@@ -28,8 +32,11 @@ public class login extends AppCompatActivity {
             }
         });
 
+
+
         logInBtn = (Button)findViewById(R.id.loginBtn);
         logInBtn.setOnClickListener(new View.OnClickListener() {
+            
             @Override
             public void onClick(View view) {
                 Intent logInIntent = new Intent(login.this,home.class);
@@ -37,4 +44,27 @@ public class login extends AppCompatActivity {
             }
         });
     }
+
+//    public boolean validate() {
+//        boolean valid = true;
+//
+//        String email = emailET.getText().toString();
+//        String password = passwordET.getText().toString();
+//
+//        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+//            emailET.setError("enter a valid email address");
+//            valid = false;
+//        } else {
+//            emailET.setError(null);
+//        }
+//
+//        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+//            passwordET.setError("between 4 and 10 alphanumeric characters");
+//            valid = false;
+//        } else {
+//            passwordET.setError(null);
+//        }
+//
+//        return valid;
+//    }
 }
