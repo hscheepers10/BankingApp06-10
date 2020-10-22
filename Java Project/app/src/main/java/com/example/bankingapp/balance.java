03756132ccf511f91eb869907345336ed0b5ffd6
Heinrich <hscheepers10@gmail.com>
2020-10-22 21:24:54 +0200

    Recon

--- Java Project/app/src/main/java/com/example/bankingapp/balance.java
+++ Java Project/app/src/main/java/com/example/bankingapp/balance.java
@@ -3,6 +3,8 @@ package com.example.bankingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class balance extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class balance extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.balance);
+
+        dbHelper myDB;
+
+        TextView name = (TextView)findViewById(R.id.nameTV);
+        TextView surname = (TextView)findViewById(R.id.surnameTV);
+        TextView cb = (TextView)findViewById(R.id.curBalTV);
+        TextView sb = (TextView)findViewById(R.id.savBalTV);
+
+
+
     }
+
 }
--- Java Project/app/src/main/java/com/example/bankingapp/dbHelper.java
+++ Java Project/app/src/main/java/com/example/bankingapp/dbHelper.java
@@ -3,6 +3,7 @@ package com.example.bankingapp;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -17,6 +18,8 @@ public class dbHelper extends SQLiteOpenHelper {
     public static final String COL_3 ="surname";
     public static final String COL_4 ="mobile";
     public static final String COL_5 ="password";
+    public static final String COL_6 ="curBal";
+    public static final String COL_7 ="savBal";
 
     public dbHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null,1);
@@ -27,7 +30,7 @@ public class dbHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqlDb) {
         sqlDb.execSQL("create table "
-                + TABLE_NAME + "(email STRING PRIMARY KEY, name TEXT, surname TEXT, mobile INTEGER, password STRING)");
+                + TABLE_NAME + "(email STRING PRIMARY KEY, name TEXT, surname TEXT, mobile INTEGER, password STRING, curBal INTEGER, savBal INTEGER)");
     }
 
     //Onupgrade Method.
@@ -38,7 +41,7 @@ public class dbHelper extends SQLiteOpenHelper {
     }
 
     //Inserting data.
-    public void addData(String email,String name,String surname, String mobile, String password){
+    public void addData(String email,String name,String surname, String mobile, String password,double curBal,double savBal){
         SQLiteDatabase sqlDb = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
         cv.put(COL_1,email);
@@ -46,6 +49,15 @@ public class dbHelper extends SQLiteOpenHelper {
         cv.put(COL_3,surname);
         cv.put(COL_4,mobile);
         cv.put(COL_5,password);
+        cv.put(COL_6,curBal);
+        cv.put(COL_7,savBal);
         long rs = sqlDb.insert(TABLE_NAME,null,cv);
+    }
+
+    //Retrieving data.
+    public Cursor getData(SQLiteDatabase sqlDb){
+        String[] values = {COL_1,COL_2,COL_3,COL_4,COL_5,COL_6,COL_7};
+        Cursor cr = sqlDb.query(dbHelper.TABLE_NAME,values,null,null,null,null,null);
+        return cr;
     }
 }
